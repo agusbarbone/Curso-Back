959cb1def781c63dab54fdd0237b2cd2be9959e4
Agustin Barbone <abarbone@gmail.com>
2023-10-30 14:20:11 -0300

    3er entregable

--- entregable3/src/app.js
+++ entregable3/src/app.js
@@ -1,23 +1,30 @@
-import express from "express"
+import express from "express";
+import ProductManager from "../../entregable2/entregable2.js";
 
-const PORT = 8080
+const PORT = 8080;
 
-const app = express()
-app.use(express.urlencoded({ extended: true }))
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/saludo', (req, res) => {
-    res.send(`Hola desde Express, desde el puerto ${PORT}`)
-})
+const manager1 = new ProductManager("../../entregable2/products.json");
 
-app.get('/user', (req, res) => {
-    const user = {
-        userName: "Agustin",
-        lastName: "Barbone",
-        age: 32
-    }
-    res.send(user)
+app.get("/products", async (req, res) => {
+  const prods = await manager1.getProducts();
+  res.send(prods);
+  console.log(prods, "llegando");
+});
+
+app.get("/products/:pid", async (req, res) => {
+  const prods = await manager1.getProducts();
+  res.send(prods[req.params.pid]);
+  console.log(prods);
+});
+
+app.get('/productquery', async (req, res) => {
+    const prods = await manager1.getProducts()
+    res.send(prods[req.query.pid])
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor de Express activo en el puerto ${PORT}`)
-})
+  console.log(`Servidor de Express activo en el puerto ${PORT}`);
+});
