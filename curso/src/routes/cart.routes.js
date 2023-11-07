d5171708f503316283e970ad1bca6ca310d3d8e2
Agustin Barbone <abarbone@gmail.com>
2023-11-07 13:12:58 -0300

    1er entregable

--- curso/src/routes/cart.routes.js
+++ curso/src/routes/cart.routes.js
@@ -1,17 +1,40 @@
 import { Router } from "express";
+import cartManager from "../../cartManager.js";
+import { __dirname } from "../utils.js";
 
 const router = Router();
 
-const cart = [];
+const CARTPATH = `${__dirname}data\\cart.json`;
+const PRODUCTPATH = `${__dirname}data\\products.json`;
 
-let idCounter = 0;
-
-router.post("/", (req, res) => {
-  res.status(200).send(req.body);
+router.post("/",async (req, res) => {
+  try {
+    const cartController = new cartManager(CARTPATH, PRODUCTPATH)
+    const cart = await cartController.addCart()
+    res.status(200).json(cart)
+  } catch (error) {
+    res.status(400).json({error: error.message })
+  }
 });
 
-router.get("/:cid", (req, res) => {});
+router.get("/:cid", async (req, res) => {
+  try {
+    const cartController = new cartManager(CARTPATH, PRODUCTPATH)
+    const cart = await cartController.getCart()
+    res.status(200).json(cart)
+  } catch (error) {
+    res.status(400).json({error: error.message })
+  }
+});
 
-router.post("/:cid/product/:pid", (req, res) => {});
+router.post("/:cid/product/:pid",async (req, res) => {
+  try {
+    const cartController = new cartManager(CARTPATH, PRODUCTPATH)
+    const cart = await cartController.addProductToCart(req.params.cid, req.params.pid)
+    res.status(200).json(cart)
+  } catch (error) {
+    res.status(400).json({error: error.message })
+  }
+});
 
 export default router;
