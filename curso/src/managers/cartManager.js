805bb7e3b948fa17f6f793daf9690f868436eb81
Agustin Barbone <abarbone@gmail.com>
2023-11-09 10:54:32 -0300

    entregable

--- curso/src/managers/cartManager.js
+++ curso/src/managers/cartManager.js
@@ -23,27 +23,22 @@ export default class CartManager {
   }
 
   async addProductToCart(cid, pid) {
-    // Obtener el carrito por su ID
     const carritoBuscado = await this.getCartById(cid);
   
-    // Obtener el producto por su ID
     await this.productManager.getProductById(pid);
-  
-    // Buscar si el producto ya existe en el carrito
+
     const existingProduct = carritoBuscado.products.find(product => product.id === pid);
   
     if (existingProduct) {
-      // Si el producto ya está en el carrito, aumenta la cantidad en 1
       existingProduct.cantidad++;
     } else {
-      // Si el producto no está en el carrito, agrégalo con cantidad 1
       carritoBuscado.products.push({ id: pid, cantidad: 1 });
     }
   
-    // Obtener la lista de carritos
+
     const arrayCarts = await this.fileSystem.getCart();
   
-    // Actualizar el carrito en la lista
+
     const updatedCartArray = arrayCarts.map((cart) => {
       if (cart.id === cid) {
         return carritoBuscado;
@@ -51,10 +46,10 @@ export default class CartManager {
       return cart;
     });
   
-    // Guardar la lista de carritos actualizada
+
     await this.fileSystem.saveCart(updatedCartArray);
   
-    // Devolver el array de carritos actualizado
+
     return updatedCartArray;
   }
 }
