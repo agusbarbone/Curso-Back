71767bee10a4e2d29b0efb5fffeb2bd9571284d1
Agustin Barbone <abarbone@gmail.com>
2023-12-01 09:13:15 -0300

    entregable

--- curso/src/dao/controllers/cart.controller.mdb.js
+++ curso/src/dao/controllers/cart.controller.mdb.js
@@ -6,13 +6,7 @@ export class CartController {
 
     async getCarts() {
         try {
-            // path: elemento a completar
             return await cartModel.find().lean()
-            // Podemos intercalar el método populate() para "completar" el array products.
-            // De esta forma, en lugar de aparecer los id en la consulta, aparecerán los datos
-            // completos de cada producto.
-            // Se puede realizar el populate aquí o directamente en el modelo (ver cart.model.js)
-            // return await cartModel.find().populate({ path: 'products', model: productModel }).lean();
         } catch (e) {
             return e.message
         }
